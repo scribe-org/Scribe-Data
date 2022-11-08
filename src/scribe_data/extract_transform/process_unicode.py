0381755bbcff6ce9c18ac48a9ee641ce8b77a66c
wkyoshida <wkyoshida.dev@gmail.com>
2022-11-07 23:41:15 -0300

    #14 initial emoji keywords

--- src/scribe_data/extract_transform/process_unicode.py
+++ src/scribe_data/extract_transform/process_unicode.py
@@ -8,6 +8,7 @@ Contents:
     gen_emoji_autosuggestions
 """
 
+import emoji
 import json
 
 from scribe_data.load.update_utils import get_language_iso
@@ -51,14 +52,26 @@ def gen_emoji_autosuggestions(
 
     iso = get_language_iso(language)
 
-    cldr_file_path = f"node_modules/cldr-annotations-derived-full/annotationsDerived/{iso}/annotations.json"
+    cldr_file_path = f"node_modules/cldr-annotations-full/annotations/{iso}/annotations.json"
 
     with open(cldr_file_path, "r") as file:
         cldr_data = json.load(file)
 
-    emoji_dict = cldr_data["annotationsDerived"]["annotations"]
+    cldr_dict = cldr_data["annotations"]["annotations"]
 
-    print("Number of emojis loaded:", len(emoji_dict))
+    print("Number of characters loaded:", len(cldr_dict))
+
+    for cldr_char in cldr_dict:
+        # Filter CLDR data for emoji characters
+        if cldr_char in emoji.EMOJI_DATA:
+            emoji_annotations = cldr_dict[cldr_char]
+            
+            for emoji_keyword in emoji_annotations["default"]:
+                # Use single-word annotations as keywords
+                if len(emoji_keyword.split()) == 1:
+                    autosuggest_dict.setdefault(emoji_keyword, []).append(cldr_char)
+
+    print("Number of trigger keywords found:", len(autosuggest_dict))
 
     ###
 
