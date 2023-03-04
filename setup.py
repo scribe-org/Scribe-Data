0c1fd30d2f2be7758fd37ea85af47cac67e4a4c5
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-03-04 12:21:31 -0700

    Version update for limiting the number of emoji suggestions

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup_args = dict(
     name="scribe-data",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    version="2.1.0",
+    version="2.1.1",
     author="Andrew Tavis McAllister",
     author_email="andrew.t.mcallister@gmail.com",
     classifiers=[
