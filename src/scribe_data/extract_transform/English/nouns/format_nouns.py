59e24224d06e89d2833bdd8ea9676f64f43bacf5
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2023-08-24 00:57:18 +0530

    format script for en_nouns along with data

--- src/scribe_data/extract_transform/English/nouns/format_nouns.py
+++ src/scribe_data/extract_transform/English/nouns/format_nouns.py
@@ -52,44 +52,49 @@ nouns_formatted = {}
 
 for noun_vals in nouns_list:
     if "singular" in noun_vals.keys():
-        if noun_vals["singluar"] not in nouns_formatted:
-            nouns_formatted[noun_vals["singular"]] = {
-                "plural": noun_vals["plural"],
-                "form": "",
-            }
+        if noun_vals["singular"] not in nouns_formatted:
+            if "plural" in noun_vals.keys():
+                nouns_formatted[noun_vals["singular"]] = {
+                    "plural": noun_vals["plural"],
+                    "form": "",
+                }
 
-            # Assign plural as a new entry after checking if it's its own plural.
-            if noun_vals["plural"] not in nouns_formatted:
-                if noun_vals["singular"] != noun_vals["plural"]:
-                    nouns_formatted[noun_vals["plural"]] = {
-                        "plural": "isPlural",
-                        "form": "PL",
-                    }
+                # Assign plural as a new entry after checking if it's its own plural.
+                if noun_vals["plural"] not in nouns_formatted:
+                    if noun_vals["singular"] != noun_vals["plural"]:
+                        nouns_formatted[noun_vals["plural"]] = {
+                            "plural": "isPlural",
+                            "form": "PL",
+                        }
 
+                    else:
+                        nouns_formatted[noun_vals["plural"]] = {
+                            "plural": noun_vals["plural"],
+                            "form": "PL",
+                        }
                 else:
-                    nouns_formatted[noun_vals["plural"]] = {
-                        "plural": noun_vals["plural"],
-                        "form": "PL",
-                    }
+                    # Mark plural as a possible form if it isn't already.
+                    if (
+                        "PL" not in nouns_formatted[noun_vals["plural"]]["form"]
+                        and nouns_formatted[noun_vals["plural"]]["form"] != ""
+                    ):
+                        nouns_formatted[noun_vals["plural"]]["form"] = (
+                            nouns_formatted[noun_vals["plural"]]["form"] + "/PL"
+                        )
+
+                    elif nouns_formatted[noun_vals["plural"]]["form"] == "":
+                        nouns_formatted[noun_vals["plural"]]["form"] = "PL"
+
+                    # Assign itself as a plural if possible (maybe wasn't for prior versions).
+                    if noun_vals["singular"] == noun_vals["plural"]:
+                        nouns_formatted[noun_vals["plural"]]["plural"] = noun_vals[
+                            "plural"
+                        ]
             else:
-                # Mark plural as a possible form if it isn't already.
-                if (
-                    "PL" not in nouns_formatted[noun_vals["plural"]]["form"]
-                    and nouns_formatted[noun_vals["plural"]]["form"] != ""
-                ):
-                    nouns_formatted[noun_vals["plural"]]["form"] = (
-                        nouns_formatted[noun_vals["plural"]]["form"] + "/PL"
-                    )
-
-                elif nouns_formatted[noun_vals["plural"]]["form"] == "":
-                    nouns_formatted[noun_vals["plural"]]["form"] = "PL"
-
-                # Assign itself as a plural if possible (maybe wasn't for prior versions).
-                if noun_vals["singular"] == noun_vals["plural"]:
-                    nouns_formatted[noun_vals["plural"]]["plural"] = noun_vals[
-                        "plural"
-                    ]
-    
+                nouns_formatted[noun_vals["singular"]] = {
+                    "plural": "",
+                    "form": "",
+                }
     elif "plural" in noun_vals.keys():
         if noun_vals["plural"] not in nouns_formatted:
             nouns_formatted[noun_vals["plural"]] = {
@@ -115,9 +120,13 @@ for k in nouns_formatted:
 nouns_formatted = collections.OrderedDict(sorted(nouns_formatted.items()))
 
 org_path = get_path_from_et_dir()
-export_path = "../formatted_data/nouns.json"
+export_dir = "../formatted_data/"
+export_path = os.path.join(export_dir, "nouns.json")
 if update_data_in_use:
     export_path = f"{org_path}/Scribe-Data/src/scribe_data/extract_transform/{LANGUAGE}/formatted_data/nouns.json"
+
+if not os.path.exists(export_dir):
+    os.makedirs(export_dir)
 
 with open(export_path, "w", encoding="utf-8",) as file:
     json.dump(nouns_formatted, file, ensure_ascii=False, indent=0)
