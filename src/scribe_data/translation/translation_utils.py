52d6e6de5aa082dff78364864d1210c48fe4e2fb
Arpita kesharwani <107834813+KesharwaniArpita@users.noreply.github.com>
2024-10-10 21:20:50 +0530

    Update translation_utils.py to remove the translate_to_other_languages function

--- src/scribe_data/translation/translation_utils.py
+++ src/scribe_data/translation/translation_utils.py
@@ -26,9 +26,6 @@ import time
 from functools import lru_cache
 from pathlib import Path
 
-import torch
-from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
-
 from scribe_data.utils import (
     DEFAULT_JSON_EXPORT_DIR,
     get_language_iso,
@@ -148,99 +145,3 @@ def translation_interrupt_handler(source_language, translations):
 
     print("The current progress is saved to the translations.json file.")
     exit()
-
-
-def translate_to_other_languages(
-    source_language: str,
-    word_list: list,
-    translations: dict,
-    batch_size: int,
-):
-    """
-    Translates a list of words from the source language to other target languages using batch processing.
-
-    Parameters
-    ----------
-        source_language : str
-            The source language being translated from.
-
-        word_list : list[str]
-            The list of words to translate.
-
-        translations : dict
-            The current dictionary of translation dictionaries.
-
-        batch_size : int
-            The number of words to translate in each batch.
-    """
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M")
-    tokenizer = M2M100Tokenizer.from_pretrained(
-        "facebook/m2m100_418M", src_lang=get_language_iso(source_language)
-    )
-
-    model.to(device)
-
-    articles_dict = get_articles_dict()
-
-    signal.signal(
-        signal.SIGINT,
-        lambda sig, frame: translation_interrupt_handler(source_language, translations),
-    )
-
-    total_time = 0
-    for i in range(0, len(word_list), batch_size):
-        start_time = time.time()
-        batch_words = word_list[i : i + batch_size]
-        batch_words = remove_articles_from_words(
-            batch_words, articles_dict[get_language_iso(source_language)]
-        )
-
-        print(f"Translating batch {i//batch_size + 1}: {batch_words}")
-
-        for lang_code in get_target_lang_codes(source_language):
-            tokenizer.src_lang = get_language_iso(source_language)
-            encoded_words = tokenizer(batch_words, return_tensors="pt", padding=True)
-            generated_tokens = model.generate(
-                **encoded_words.to(device),
-                forced_bos_token_id=tokenizer.get_lang_id(lang_code),
-            )
-            translated_words = tokenizer.batch_decode(
-                generated_tokens, skip_special_tokens=True
-            )
-            translated_words_cleaned = remove_articles_from_words(
-                translated_words, articles_dict[lang_code]
-            )
-
-            for word, translation in zip(batch_words, translated_words_cleaned):
-                if word not in translations:
-                    translations[word] = {}
-
-                translations[word][lang_code] = translation
-
-        batch_time = time.time() - start_time
-        total_time += batch_time
-        decimal_completed = (i + batch_size) / len(word_list)
-        percent_completed = round(decimal_completed, 4) * 100
-        time_to_finish_estimate = round(total_time / decimal_completed / 60 / 60, 4)
-
-        print(
-            f"Batch {i // batch_size + 1} translations completed - {percent_completed}% complete"
-        )
-        print(f"Time to finish batch: {batch_time} seconds")
-        print(
-            f"Time to finish {source_language} translations: {time_to_finish_estimate} hours\n"
-        )
-
-        with open(
-            Path(DEFAULT_JSON_EXPORT_DIR) / source_language / "translations.json",
-            "w",
-            encoding="utf-8",
-        ) as file:
-            file.write(json.dumps(translations, ensure_ascii=False, indent=0))
-            file.write("\n")
-
-    print(
-        f"Translation results for all words are saved to the {source_language}/translations.json file."
-    )
