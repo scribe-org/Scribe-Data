d3793b9b9460189c3912d00d21de0107039e154f
Ekikereabasi Nkereuwem <ekikerenkereuwem@gmail.com>
2024-10-16 07:54:46 +0100

    Update generate_emoji_keyword.py with additional changes

--- src/scribe_data/language_data_extraction/Hindustani/Hindi/emoji_keywords/generate_emoji_keywords.py
+++ src/scribe_data/language_data_extraction/Hindustani/Hindi/emoji_keywords/generate_emoji_keywords.py
@@ -1,47 +1,57 @@
 """
-Generates keyword-emoji relationships from a selection of Hindi words, ensuring Urdu words are excluded.
-
-.. raw:: html
-    <!--
-    * Copyright (C) 2024 Scribe
-    *
-    * This program is free software: you can redistribute it and/or modify
-    * it under the terms of the GNU General Public License as published by
-    * the Free Software Foundation, either version 3 of the License, or
-    * (at your option) any later version.
-    *
-    * This program is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    * GNU General Public License for more details.
-    *
-    * You should have received a copy of the GNU General Public License
-    * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    -->
+* Copyright (C) 2024 Scribe
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import argparse
+from scribe_data.unicode.generate_emoji_keyword import generate_emoji_keyword
 
-from scribe_data.unicode.process_unicode import gen_emoji_lexicon
-from scribe_data.utils import export_formatted_data
-
-LANGUAGE = "Hindi"
-DATA_TYPE = "emoji-keywords"
+# Define the main language
+LANGUAGE = "Hindustani"  # Change to a grouped language if needed
 emojis_per_keyword = 3
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--file-path")
+# Set up the argument parser
+parser = argparse.ArgumentParser(
+    description="Generate emoji keywords for a specific language."
+)
+parser.add_argument(
+    "--file-path", required=True, help="Path to save the generated emoji keywords."
+)
+parser.add_argument(
+    "--sub-languages",
+    nargs="*",
+    help="List of specific sub-languages to process (e.g., Hindi Urdu). If omitted, all sub-languages will be processed.",
+)
+parser.add_argument(
+    "--gender",
+    choices=["male", "female", "neutral"],
+    help="Specify the gender for emoji customization.",
+)
+parser.add_argument(
+    "--region", help="Specify the region for emoji customization (e.g., US, IN)."
+)
+
+# Parse the command-line arguments
 args = parser.parse_args()
 
-# Generating emoji lexicon but filter for Hindi specifically
-if emoji_keywords_dict := gen_emoji_lexicon(
-    language=LANGUAGE,
-    emojis_per_keyword=emojis_per_keyword,
-):
-    export_formatted_data(
-        file_path=args.file_path,
-        formatted_data=emoji_keywords_dict,
-        query_data_in_use=True,
-        language="Hindi",  # Setting final language to Hindi for output purposes
-        data_type=DATA_TYPE,
-    )
+# Call the generate_emoji_keyword function with optional parameters
+generate_emoji_keyword(
+    LANGUAGE,
+    emojis_per_keyword,
+    args.file_path,
+    gender=args.gender,
+    region=args.region,
+    sub_languages=args.sub_languages,
+)
