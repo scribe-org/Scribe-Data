d588922352ef690e7064c6e6cc281b77bba08863
Omar Agiez <omaragiez3@gmail.com>
2024-10-18 13:00:58 +0300

    Fix: Remove deprecated theme path call in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@ import sys
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
@@ -91,7 +90,7 @@ pygments_style = "sphinx"
 
 html_theme = "sphinx_rtd_theme"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
