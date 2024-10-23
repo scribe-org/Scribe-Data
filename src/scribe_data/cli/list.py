a6173c40bd1d22511cacc3eedac3e3b88c5a2cf3
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-10-24 00:50:50 +0200

    Fix directory structure call to fix tests

--- src/scribe_data/cli/list.py
+++ src/scribe_data/cli/list.py
@@ -77,10 +77,10 @@ def list_data_types(language: str = None) -> None:
     if language:
         language = format_sublanguage_name(language, language_metadata)
         language_data = language_map.get(language.lower())
-        language_dir = LANGUAGE_DATA_EXTRACTION_DIR / language
+        language_dir = LANGUAGE_DATA_EXTRACTION_DIR / language.lower()
 
         if not language_data:
-            raise ValueError(f"Language '{language}' is not recognized.")
+            raise ValueError(f"Language '{language.capitalize()}' is not recognized.")
 
         data_types = {f.name for f in language_dir.iterdir() if f.is_dir()}
 
