fbdce9301ab387c9221510d841dddb4583373c68
Deadpool2000 <d2kyt@protonmail.com>
2025-01-20 22:57:23 +0530

    Standardize SPDX license declaration across all Python files

--- src/scribe_data/unicode/update_unicode_data.py
+++ src/scribe_data/unicode/update_unicode_data.py
@@ -5,24 +5,7 @@ Example
 -------
     python3 src/scribe_data/unicode/update_unicode_data.py
 
-.. raw:: html
-
-    <!--
-    * Copyright (C) 2024 Scribe
-    *
-    * This program is free software: you can redistribute it and/or modify
-    * it under the terms of the GNU General Public License as published by
-    * the Free Software Foundation, either version 3 of the License, or
-    * (at your option) any later version.
-    *
-    * This program is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    * GNU General Public License for more details.
-    *
-    * You should have received a copy of the GNU General Public License
-    * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    -->
+# SPDX-License-Identifier: AGPL-3.0-or-later
 """
 
 import os
