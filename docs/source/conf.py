28c797c90c8a718d156268d5cc2169c5345d78d8
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2025-02-10 00:50:59 +0100

    Update header of docs/source/conf.py with license

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
