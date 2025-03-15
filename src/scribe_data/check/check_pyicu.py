0768ef665d42f95933488ec3d61e9486f6f8c5a9
axif <muhamadasif570@gmail.com>
2025-03-16 05:06:09 +0600

    Refactor package version checks to use importlib.metadata instead of pkg_resources. Update tests accordingly and add new tests for data handling in SQLite.

--- src/scribe_data/check/check_pyicu.py
+++ src/scribe_data/check/check_pyicu.py
@@ -6,22 +6,24 @@ Example
 -------
     python3 src/scribe_data/check/check_pyicu.py
 """
-
+import importlib.metadata
 import os
-import platform  # added to check the OS
+import platform
 import subprocess
 import sys
 from pathlib import Path
 
-import pkg_resources
 import questionary
 import requests
 
 
 def check_if_pyicu_installed():
-    installed_packages = {pkg.key for pkg in pkg_resources.working_set}
-
-    return "pyicu" in installed_packages
+    try:
+        # Check if PyICU is installed using importlib.metadata.
+        importlib.metadata.version("PyICU")
+        return True
+    except importlib.metadata.PackageNotFoundError:
+        return False
 
 
 def get_python_version_and_architecture():
@@ -130,8 +132,11 @@ def find_matching_wheel(wheels, python_version, architecture):
 
 def check_and_install_pyicu():
     package_name = "PyICU"
-    installed_packages = {pkg.key for pkg in pkg_resources.working_set}
-    if package_name.lower() not in installed_packages:
+    try:
+        version = importlib.metadata.version(package_name)
+        print(f"PyICU version: {version}")
+        return True
+    except importlib.metadata.PackageNotFoundError:
         # Fetch available wheels from GitHub to estimate download size.
         wheels, total_size_mb = fetch_wheel_releases()
 
@@ -194,3 +199,6 @@ def check_and_install_pyicu():
                 return False
 
     return True
+
+if __name__ == "__main__":
+    check_and_install_pyicu()
