717e471b9e09f7c3ac6f90998273d25d33e57f90
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2025-08-24 20:45:21 +0200

    Fix comments in project metadata tests

--- tests/check/test_check_project_metadata.py
+++ tests/check/test_check_project_metadata.py
@@ -18,7 +18,7 @@ class TestCheckProjectMetadata(unittest.TestCase):
     def test_get_available_languages(self):
         """
         Tests that get_available_languages returns a dictionary with
-        languages from LANGUAGE_DATA_EXTRACTION_DIR
+        languages from LANGUAGE_DATA_EXTRACTION_DIR.
         """
         available_languages = get_available_languages()
         desired_dict = {
@@ -71,13 +71,14 @@ class TestCheckProjectMetadata(unittest.TestCase):
             if len(available_languages[lang]) > 0:
                 for sub_lang in available_languages[lang]["sub_languages"]:
                     self.assertIn(sub_lang, desired_dict[lang]["sub_languages"])
+
         self.assertEqual(len(available_languages), len(desired_dict))
 
     def test_get_missing_languages(self):
         """
         Tests that get_missing_languages returns a list of languages missing given
         a target language dictionary to check for and a reference language
-        dictionary to check in
+        dictionary to check in.
         """
         reference_languages = {
             "estonian": {},
@@ -96,20 +97,20 @@ class TestCheckProjectMetadata(unittest.TestCase):
             },
         }
 
-        # no missing languages
+        # No missing languages.
         self.assertEqual(
             get_missing_languages(reference_languages, target_languages), []
         )
 
-        # more reference languages than target languages; should have no effect
+        # More reference languages than target languages; should have no effect.
         reference_languages["basque"] = {}
-        # missing childless language
+        # Missing childless language.
         target_languages["arabic"] = {"iso": "ar", "qid": "Q13955"}
-        # missing parent language
+        # Missing parent language.
         target_languages["chinese"] = {
             "sub_languages": {"mandarin": {"iso": "zh", "qid": "Q727694"}}
         }
-        # missing only sub-languages
+        # Missing only sub-languages.
         target_languages["estonian"] = {
             "sub_languages": {"sub_estonian": {"iso": "nb", "qid": "Q25167"}}
         }
@@ -121,7 +122,7 @@ class TestCheckProjectMetadata(unittest.TestCase):
     def test_validate_language_properties(self):
         """
         Tests that validate_language_properties identifies languages missing an 'iso' and/or
-        'qid' field from a dictionary of languages
+        'qid' field from a dictionary of languages.
         """
         languages = {
             "arabic": {"iso": "ar", "qid": "Q13955"},
@@ -130,26 +131,26 @@ class TestCheckProjectMetadata(unittest.TestCase):
             "chinese": {"sub_languages": {"mandarin": {"iso": "zh", "qid": "Q727694"}}},
         }
 
-        # no missing "iso" or "qid"
+        # No missing "iso" or "qid".
         self.assertEqual(
             validate_language_properties(languages),
             {"missing_qids": [], "missing_isos": []},
         )
 
-        # childless language missing "iso"
+        # Childless language missing "iso".
         languages["persian"] = {"qid": "Q9168"}
-        # childless language missing "qid"
+        # Childless language missing "qid".
         languages["russian"] = {"iso": "ru"}
-        # childless language missing "iso" and "qid"
+        # Childless language missing "iso" and "qid".
         languages["tajik"] = {}
-        # sub-language missing "iso" and sub-language missing "qid"
+        # Sub-language missing "iso" and sub-language missing "qid".
         languages["punjabi"] = {
             "sub_languages": {
                 "gurmukhi": {"qid": "Q58635"},
                 "shahmukhi": {"iso": "pnb"},
             }
         }
-        # sub-language missing "iso" and "qid"
+        # Sub-language missing "iso" and "qid".
         languages["chinese"]["sub_languages"]["mandarin"] = {}
         self.assertEqual(
             validate_language_properties(languages),
@@ -171,7 +172,7 @@ class TestCheckProjectMetadata(unittest.TestCase):
 
     def test_check_language_metadata_success_case(self):
         """
-        Tests that check_language_metadata runs successfully
+        Tests that check_language_metadata runs successfully.
         """
         with patch("sys.stdout", new=StringIO()) as out:
             check_language_metadata()
@@ -185,7 +186,7 @@ class TestCheckProjectMetadata(unittest.TestCase):
         """
         Tests that check_language_metadata prints the correct error message when
         the language metadata has languages missing from the language data extraction
-        and languages without 'qid' and or 'iso' fields
+        and languages without 'qid' and or 'iso' fields.
         """
         with (
             patch(
